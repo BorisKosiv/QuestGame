daf530dd8d444a6a9dba99d4f0daccd77f4d587a
boris <bkosiv@gmail.com>
2024-02-06 17:02:54 +0200

    Added gaem1.jsp, game2.jsp, gameover.jsp, GameOverServlet.java.

--- src/main/java/com/company/questgame/filter/GameFilter.java
+++ src/main/java/com/company/questgame/filter/GameFilter.java
@@ -14,24 +14,17 @@ public class GameFilter implements Filter {
     public void init(FilterConfig filterConfig)  {
         System.out.println("GameFilter init");
     }
-
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        //String lang = request.getParameter("lang");
-
             Player player = new Player();
             HttpSession session = request.getSession();
-          //session.setAttribute("lang", lang);
             session.setAttribute("player", player);
             request.getRequestDispatcher("/game").forward(request, response);
             filterChain.doFilter(request, response);
-
-
     }
-
     @Override
     public void destroy() {
 
